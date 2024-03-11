365836dfa6bfb331c6401b67da9ce67c338e0cbb
xdgorgola <pedro18fe@gmail.com>
2024-03-11 19:45:38 -0400

    a3 shortened

--- T7/a3.py
+++ T7/a3.py
@@ -1,4 +1,4 @@
-def precond(w: str) -> tuple[list[int], int]:
+def lps(w: str) -> tuple[list[int], int]:
     b = [-1 for _ in range(len(w))]
     j = -1
     for i in range(1,len(w)):
@@ -8,14 +8,8 @@ def precond(w: str) -> tuple[list[int], int]:
         b[i] = j
         if j == 0:
             ps = i
-    return (b, ps)
-
-
-def TS(w: str) -> str:
-    (_, ps) = precond(w)
     return "" if w[0] != w[ps] and ps > 0 else w[ps:]
-
 
 ws = ["SEVENTY SEVEN ", "abracadabra", "arepera", "algoritmo"]
 for w in ws:
-    print(TS(w))
+    print(lps(w))
